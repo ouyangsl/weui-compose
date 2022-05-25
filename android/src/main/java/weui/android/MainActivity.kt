f54f2cfef6dfe0ac812ab3624d7dafdb2b1cfa8b
Ouyang <ouyangsilun@hunuokeji.cn>
2022-05-25 20:52:39 +0800

    完善sample

--- android/src/main/java/weui/android/MainActivity.kt
+++ android/src/main/java/weui/android/MainActivity.kt
@@ -4,15 +4,13 @@ import weui.sample.App
 import android.os.Bundle
 import androidx.activity.compose.setContent
 import androidx.appcompat.app.AppCompatActivity
-import androidx.compose.material.MaterialTheme
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         setContent {
-            MaterialTheme {
-                App()
-            }
+            App()
         }
     }
 }
