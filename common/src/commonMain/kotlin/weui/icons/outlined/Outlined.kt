4788a1bee93c29602f475763f3e5a79c2bfff2a7
Ouyang <ouyangsilun@hunuokeji.cn>
2022-05-25 10:34:42 +0800

    添加图标资源

---
+++ common/src/commonMain/kotlin/weui/icons/outlined/Outlined.kt
@@ -0,0 +1,173 @@
+package weui.icons.outlined
+
+import weui.theme.WeUI
+
+/**
+ * Outlined 风格的图标
+ */
+
+private const val Prefix = "icons/outlined/"
+private const val Suffix = ".svg"
+
+val WeUI.Icons.AddFriends: String
+    get() = "${Prefix}add-friends${Suffix}"
+val WeUI.Icons.Add: String
+    get() = "${Prefix}add${Suffix}"
+val WeUI.Icons.Add2: String
+    get() = "${Prefix}add2${Suffix}"
+val WeUI.Icons.Album: String
+    get() = "${Prefix}album${Suffix}"
+val WeUI.Icons.Arrow: String
+    get() = "${Prefix}arrow${Suffix}"
+val WeUI.Icons.At: String
+    get() = "${Prefix}at${Suffix}"
+val WeUI.Icons.Back: String
+    get() = "${Prefix}back${Suffix}"
+val WeUI.Icons.Back2: String
+    get() = "${Prefix}back2${Suffix}"
+val WeUI.Icons.BellringOff: String
+    get() = "${Prefix}bellring-off${Suffix}"
+val WeUI.Icons.BellringOn: String
+    get() = "${Prefix}bellring-on${Suffix}"
+val WeUI.Icons.Camera: String
+    get() = "${Prefix}camera${Suffix}"
+val WeUI.Icons.Cellphone: String
+    get() = "${Prefix}cellphone${Suffix}"
+val WeUI.Icons.Clip: String
+    get() = "${Prefix}clip${Suffix}"
+val WeUI.Icons.Close: String
+    get() = "${Prefix}close${Suffix}"
+val WeUI.Icons.Close2: String
+    get() = "${Prefix}close2${Suffix}"
+val WeUI.Icons.Comment: String
+    get() = "${Prefix}comment${Suffix}"
+val WeUI.Icons.Contacts: String
+    get() = "${Prefix}contacts${Suffix}"
+val WeUI.Icons.Copy: String
+    get() = "${Prefix}copy${Suffix}"
+val WeUI.Icons.DeleteOn: String
+    get() = "${Prefix}delete-on${Suffix}"
+val WeUI.Icons.Delete: String
+    get() = "${Prefix}delete${Suffix}"
+val WeUI.Icons.Discover: String
+    get() = "${Prefix}discover${Suffix}"
+val WeUI.Icons.Display: String
+    get() = "${Prefix}display${Suffix}"
+val WeUI.Icons.Done: String
+    get() = "${Prefix}done${Suffix}"
+val WeUI.Icons.Done2: String
+    get() = "${Prefix}done2${Suffix}"
+val WeUI.Icons.Download: String
+    get() = "${Prefix}download${Suffix}"
+val WeUI.Icons.Email: String
+    get() = "${Prefix}email${Suffix}"
+val WeUI.Icons.Error: String
+    get() = "${Prefix}error${Suffix}"
+val WeUI.Icons.EyesOff: String
+    get() = "${Prefix}eyes-off${Suffix}"
+val WeUI.Icons.EyesOn: String
+    get() = "${Prefix}eyes-on${Suffix}"
+val WeUI.Icons.Folder: String
+    get() = "${Prefix}folder${Suffix}"
+val WeUI.Icons.GroupDetail: String
+    get() = "${Prefix}group-detail${Suffix}"
+val WeUI.Icons.Help: String
+    get() = "${Prefix}help${Suffix}"
+val WeUI.Icons.Home: String
+    get() = "${Prefix}home${Suffix}"
+val WeUI.Icons.Imac: String
+    get() = "${Prefix}imac${Suffix}"
+val WeUI.Icons.Info: String
+    get() = "${Prefix}info${Suffix}"
+val WeUI.Icons.Keyboard: String
+    get() = "${Prefix}keyboard${Suffix}"
+val WeUI.Icons.Like: String
+    get() = "${Prefix}like${Suffix}"
+val WeUI.Icons.Link: String
+    get() = "${Prefix}link${Suffix}"
+val WeUI.Icons.Location: String
+    get() = "${Prefix}location${Suffix}"
+val WeUI.Icons.Lock: String
+    get() = "${Prefix}lock${Suffix}"
+val WeUI.Icons.MaxWindow: String
+    get() = "${Prefix}max-window${Suffix}"
+val WeUI.Icons.Me: String
+    get() = "${Prefix}me${Suffix}"
+val WeUI.Icons.Mike: String
+    get() = "${Prefix}mike${Suffix}"
+val WeUI.Icons.Mike2: String
+    get() = "${Prefix}mike2${Suffix}"
+val WeUI.Icons.MobileContacts: String
+    get() = "${Prefix}mobile-contacts${Suffix}"
+val WeUI.Icons.More: String
+    get() = "${Prefix}more${Suffix}"
+val WeUI.Icons.More2: String
+    get() = "${Prefix}more2${Suffix}"
+val WeUI.Icons.Mosaic: String
+    get() = "${Prefix}mosaic${Suffix}"
+val WeUI.Icons.MusicOff: String
+    get() = "${Prefix}music-off${Suffix}"
+val WeUI.Icons.Music: String
+    get() = "${Prefix}music${Suffix}"
+val WeUI.Icons.Note: String
+    get() = "${Prefix}note${Suffix}"
+val WeUI.Icons.Pad: String
+    get() = "${Prefix}pad${Suffix}"
+val WeUI.Icons.Pause: String
+    get() = "${Prefix}pause${Suffix}"
+val WeUI.Icons.Pencil: String
+    get() = "${Prefix}pencil${Suffix}"
+val WeUI.Icons.PhotoWall: String
+    get() = "${Prefix}photo-wall${Suffix}"
+val WeUI.Icons.Play: String
+    get() = "${Prefix}play${Suffix}"
+val WeUI.Icons.Play2: String
+    get() = "${Prefix}play2${Suffix}"
+val WeUI.Icons.Previous: String
+    get() = "${Prefix}previous${Suffix}"
+val WeUI.Icons.Previous2: String
+    get() = "${Prefix}previous2${Suffix}"
+val WeUI.Icons.QrCode: String
+    get() = "${Prefix}qr-code${Suffix}"
+val WeUI.Icons.Refresh: String
+    get() = "${Prefix}refresh${Suffix}"
+val WeUI.Icons.ReportProblem: String
+    get() = "${Prefix}report-problem${Suffix}"
+val WeUI.Icons.Search: String
+    get() = "${Prefix}search${Suffix}"
+val WeUI.Icons.Sending: String
+    get() = "${Prefix}sending${Suffix}"
+val WeUI.Icons.Setting: String
+    get() = "${Prefix}setting${Suffix}"
+val WeUI.Icons.Share: String
+    get() = "${Prefix}share${Suffix}"
+val WeUI.Icons.Shop: String
+    get() = "${Prefix}shop${Suffix}"
+val WeUI.Icons.Star: String
+    get() = "${Prefix}star${Suffix}"
+val WeUI.Icons.Sticker: String
+    get() = "${Prefix}sticker${Suffix}"
+val WeUI.Icons.Tag: String
+    get() = "${Prefix}tag${Suffix}"
+val WeUI.Icons.Text: String
+    get() = "${Prefix}text${Suffix}"
+val WeUI.Icons.Time: String
+    get() = "${Prefix}time${Suffix}"
+val WeUI.Icons.TransferText: String
+    get() = "${Prefix}transfer-text${Suffix}"
+val WeUI.Icons.Transfer: String
+    get() = "${Prefix}transfer${Suffix}"
+val WeUI.Icons.Transfer2: String
+    get() = "${Prefix}transfer2${Suffix}"
+val WeUI.Icons.Tv: String
+    get() = "${Prefix}tv${Suffix}"
+val WeUI.Icons.VideoCall: String
+    get() = "${Prefix}video-call${Suffix}"
+val WeUI.Icons.Voice: String
+    get() = "${Prefix}voice${Suffix}"
+val WeUI.Icons.VolumeDown: String
+    get() = "${Prefix}volume-down${Suffix}"
+val WeUI.Icons.VolumeOff: String
+    get() = "${Prefix}volume-off${Suffix}"
+val WeUI.Icons.VolumeUp: String
+    get() = "${Prefix}volume-up${Suffix}"
