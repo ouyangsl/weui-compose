a47a0f50dee4de63b07bf3ba64e22af680257191
Ouyang <ouyangsilun@hunuokeji.cn>
2022-05-25 10:44:34 +0800

    添加图片资源绘制

---
+++ common/src/desktopMain/kotlin/weui/core/DesktopResources.kt
@@ -0,0 +1,8 @@
+package weui.core
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.res.painterResource
+
+@Composable
+actual fun imageResource(res: String): Painter = painterResource(res)
